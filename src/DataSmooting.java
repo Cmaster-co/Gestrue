8ae77d42b3640e27dbd77197380c1b323ca9bc1a
cmaster-co <1131252064@qq.com>
2015-11-24 12:56:08 +0800

    add new method Gaussion(double[] , int)

--- src/DataSmooting.java
+++ src/DataSmooting.java
@@ -1,19 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataSmooting {
-	public double average(double[] a){
-		int length = a.length;
+	//求传入数组的平均值
+	public double average(List<Double> a){
+		int length = a.size();
 		double sum = 0;
 		double average = 0;
 		if (length == 0)
 			return 0;
-		for(int i=0; i < length; i++)
+		for(double each : a)
 		{
-			sum += a[i];
+			sum += each;
 		}
 		average = sum / length;
 		return average;
 	}
-	public double[] Gaussian(){
-		return null;
+	//邻域平滑滤波
+	public double[] Gaussian(double[] a, int n){
+		int length = a.length;
+		double average = 0 ;
+		List list_out = new ArrayList();
+		if (length == 0)
+			return null;
+		for(int i=0; i < length; i++)
+		{
+			List temp = new ArrayList();
+			for(int j=i-n; j < i+n; j++)
+			{
+				if(j >= 0 && j < length)
+				{
+					temp.add(a[j]);
+				}
+			}
+			average = average(temp);
+			list_out.add(average);
+		}
+		double[] b = new double[list_out.size()];
+		for(int i=0; i < list_out.size(); i++)
+		{
+			b[i]=(Double) list_out.get(i);
+		}
+		return b;
 	}
 }
