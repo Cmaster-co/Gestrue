b06c0c00a75e44b04efa7c8c201cc1c9817f24ad
cmaster-co <1131252064@qq.com>
2015-11-26 14:12:31 +0800

    add  method 'isUser' for Authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -4,7 +4,48 @@ import java.util.List;
 public class Authentication {
 	//认证功能
 	public boolean isUser(List a, List b){
-		return false;
+		double[] ax = (double[]) a.get(0);
+		double[] ay = (double[]) a.get(1);
+		double[] az = (double[]) a.get(2);
+		double[] bx = (double[]) b.get(0);
+		double[] by = (double[]) b.get(1);
+		double[] bz = (double[]) b.get(2);
+		//识别点个数认证
+		if(ax.length != bx.length || ay.length != by.length || az.length != bz.length)
+		{
+			System.out.println("length didn't match");
+			return false;
+		}
+		int xl=ax.length;
+		int yl=ay.length;
+		int zl=az.length;
+		//识别点具体认证
+		for(int i=0; i < xl; i++)
+		{
+			if(ax[i] > bx[i] + 1 || ax[i] < bx[i] -1)
+			{
+				System.out.println("x didnt match");
+				return false;
+			}
+		}
+		for(int i=0; i < yl; i++)
+		{
+			if(ay[i] > by[i] + 1 || ay[i] < by[i] -1)
+			{
+				System.out.println("y didnt match");
+				return false;
+			}
+		}
+		for(int i=0; i < zl; i++)
+		{
+			if(az[i] > bz[i] + 1 || az[i] < bz[i] -1)
+			{
+				System.out.println("z didnt match");
+				return false;
+			}
+		}
+		System.out.println("all match");
+		return true;
 	}
 	//整合x,y,z轴上的识别点
 	public List getPeaks(double[] a, double[] b, double[] c){
